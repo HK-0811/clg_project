5fe4285b922b16c51142877448b40a2295b4aa61
Himanshu Kotkar <Himanshukotkar007@gmail.com>
2025-02-28 23:28:32 +0530

    Fixed errors

--- app.py
+++ app.py
@@ -12,12 +12,6 @@ from langchain.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
 
-# Custom CSS to enhance the UI
-import asyncio
-import sys
-
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 def get_vectorstore(url):
     # Get the url text
